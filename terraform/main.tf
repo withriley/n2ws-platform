9880387408bf9f588732dc709dcdaf9f79bc58fb
Emile Hofsink <emile.hofsink@withriley.com>
2022-03-16 10:05:49 +1100

    fixed security concerns

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,8 +36,8 @@ resource "aws_security_group" "main" {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    cidr_blocks      = ["0.0.0.0/0"] #tfsec:ignore:aws-vpc-no-public-egress-sgr
+    ipv6_cidr_blocks = ["::/0"] #tfsec:ignore:aws-vpc-no-public-egress-sgr
     prefix_list_ids  = [aws_vpc_endpoint.s3.prefix_list_id]
   }
 }
@@ -111,8 +111,31 @@ resource "aws_vpc_endpoint" "ebs" {
 }
 
 # S3 Bucket
-resource "aws_s3_bucket" "main" {
-  bucket = "n2ws-bucket"
+resource "aws_s3_bucket" "main" { #tfsec:ignore:aws-s3-encryption-customer-key
+  bucket = "n2ws-bucket" 
+} 
+
+resource "aws_s3_bucket_public_access_block" "main" {
+  bucket = aws_s3_bucket.main.id
+  block_public_acls   = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_acl" "main" {
+  bucket = aws_s3_bucket.main.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" { #tfsec:ignore:aws-s3-encryption-customer-key
+  bucket = aws_s3_bucket.main.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
+    }
+  }
 }
 
 
