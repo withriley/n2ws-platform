f8ff5080e4127286060741707b22b99e3103c436
Emile Hofsink <emile.hofsink@withriley.com>
2022-03-16 12:11:06 +1100

    fixed file references :sparkles:

--- terraform/data.tf
+++ terraform/data.tf
@@ -4,9 +4,9 @@ data "aws_region" "current" {}
 
 data "local_file" "policies" {
   for_each = toset(local.permission_policies)
-  filename = "policies/${each.value}.json"
+  filename = "${path.module}/policies/${each.value}.json"
 }
 
 data "local_file" "assume_role_policy" {
-  filename = "policies/trust_relationship.json"
+  filename = "${path.module}/policies/trust_relationship.json"
 }
