fbe6f6a410b6e6e0300489ba1d14c790818ae53e
timwestgarth <78826990+timwestgarth@users.noreply.github.com>
2023-08-26 10:24:59 +1000

    Update locals.tf
    Update locals.tf to include "aws_policy_permissions_Enterprise_BYOL_3"

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,6 +1,7 @@
 locals {
   permission_policies = [
     "aws_policy_permissions_Enterprise_BYOL_1",
-    "aws_policy_permissions_Enterprise_BYOL_2"
+    "aws_policy_permissions_Enterprise_BYOL_2",
+    "aws_policy_permissions_Enterprise_BYOL_3"
   ]
 }
